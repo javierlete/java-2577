c54da309a257330149776f6218095ca65da1a54a
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-11-13 10:47:56 +0100

    Implementación de las operaciones con logger incluído

---
+++ amazonia/src/main/java/com/amazonia/logicanegocio/UsuarioNegocioImpl.java
@@ -0,0 +1,38 @@
+package com.amazonia.logicanegocio;
+
+import java.util.logging.Logger;
+
+import com.amazonia.accesodatos.DaoProducto;
+import com.amazonia.accesodatos.DaoProductoMemoria;
+import com.amazonia.entidades.Producto;
+
+public class UsuarioNegocioImpl implements UsuarioNegocio {
+	private static final DaoProducto dao = new DaoProductoMemoria();
+	
+	private static final Logger log = Logger.getLogger(UsuarioNegocioImpl.class.getName());
+	
+	@Override
+	public Iterable<Producto> listadoProductos() {
+		log.info("Se ha pedido un listado de productos");
+		Iterable<Producto> productos = dao.obtenerTodos();
+		log.info("Se ha obtenido un listado de productos");
+		
+		return productos;
+	}
+
+	@Override
+	public Producto datosProducto(Long id) {
+		log.info("Se ha pedido el producto cuyo id es " + id);
+		
+		Producto producto = dao.obtenerPorId(id);
+		
+		if(producto == null) {
+			log.warning("No se ha encontrado el producto id " + id);
+		}
+		
+		log.info("Se ha obtenido el producto " + producto);
+		
+		return producto;
+	}
+
+}
