54f1baaf28985d1a1657ea2a672f9eae3465e85b
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-11-20 11:18:56 +0100

    Excepción de acceso a datos

---
+++ amazonia2/src/main/java/com/amazonia2/accesodatos/AccesoDatosException.java
@@ -0,0 +1,26 @@
+package com.amazonia2.accesodatos;
+
+public class AccesoDatosException extends RuntimeException {
+	private static final long serialVersionUID = -9149404930423336559L;
+
+	public AccesoDatosException() {
+		super();
+	}
+
+	public AccesoDatosException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public AccesoDatosException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public AccesoDatosException(String message) {
+		super(message);
+	}
+
+	public AccesoDatosException(Throwable cause) {
+		super(cause);
+	}
+}
