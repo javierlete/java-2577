5ebc0cb6514fa0fa09cd05a61c541c4b35aed215
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-11-20 09:11:17 +0100

    Dao genérico

---
+++ amazonia2/src/main/java/com/amazonia2/accesodatos/Dao.java
@@ -0,0 +1,10 @@
+package com.amazonia2.accesodatos;
+
+public interface Dao<T> {
+	Iterable<T> obtenerTodos();
+	T obtenerPorId(Long id);
+	
+	T insertar(T objeto);
+	T modificar(T objeto);
+	void borrar(Long id);
+}
