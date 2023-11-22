c9b4860c9a0734edc590df45c05cc141017c2f48
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-11-22 10:38:03 +0100

    Vista de ejemplo de detalle vacía

---
+++ amazonia2/src/main/java/com/amazonia2/presentacion/backend/controladores/DetalleServlet.java
@@ -0,0 +1,25 @@
+package com.amazonia2.presentacion.backend.controladores;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/detalle")
+public class DetalleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// response.getWriter().append(Global.UN.listadoProductos().toString());
+
+		request.getRequestDispatcher("/WEB-INF/vistas/detalle.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ amazonia2/src/main/webapp/WEB-INF/vistas/detalle.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ include file="/WEB-INF/vistas/includes/cabecera.jsp"%>
+
+<main class="container">
+
+	<h1>Detalle de producto</h1>
+
+</main>
+
+<%@ include file="/WEB-INF/vistas/includes/pie.jsp"%>
