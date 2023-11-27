f266c08a86611b0a2407de7c3cce8ed7b583b93c
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-11-27 09:28:56 +0100

    Carrito servlet que agrega o elimina productos de carrito

---
+++ amazonia2/src/main/java/com/amazonia2/presentacion/backend/controladores/CarritoServlet.java
@@ -0,0 +1,39 @@
+package com.amazonia2.presentacion.backend.controladores;
+
+import java.io.IOException;
+
+import com.amazonia2.entidades.Carrito;
+import com.amazonia2.globales.Global;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/carrito")
+public class CarritoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String borrar = request.getParameter("borrar");
+		String sId = request.getParameter("id");
+		
+		Carrito carrito = (Carrito) request.getSession().getAttribute("carrito");
+		
+		Long id = Long.parseLong(sId);
+		
+		if(borrar == null) {
+			carrito.addProducto(Global.UN.detalleProducto(id));
+		} else {
+			carrito.removeProducto(id);
+		}
+		
+		request.getRequestDispatcher("/WEB-INF/vistas/carrito.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
