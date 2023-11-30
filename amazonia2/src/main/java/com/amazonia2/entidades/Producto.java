00165dab40356990f09ed975cf6bc6eba5eb65bb
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-11-30 12:27:44 +0100

    Producto configurado como entidad JPA

--- amazonia2/src/main/java/com/amazonia2/entidades/Producto.java
+++ amazonia2/src/main/java/com/amazonia2/entidades/Producto.java
@@ -3,6 +3,13 @@ package com.amazonia2.entidades;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,11 +19,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 @Builder
+@Entity
+@Table(name = "productos")
 public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name = "codigo_barras")
 	private String codigoBarras;
 	private String nombre;
 	private BigDecimal precio;
+	@Column(name = "fecha_caducidad")
 	private LocalDate fechaCaducidad;
 	private Integer unidades;
 }
