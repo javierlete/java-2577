823b88149987e117daab462c93f81f5da5138079
Curso <Curso@DESKTOP-MHK683Q.ipartekaula.com>
2023-12-01 09:55:16 +0100

    Rol con JPA

--- amazonia2/src/main/java/com/amazonia2/entidades/Rol.java
+++ amazonia2/src/main/java/com/amazonia2/entidades/Rol.java
@@ -1,5 +1,10 @@
 package com.amazonia2.entidades;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +14,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 @Builder
+@Entity
+@Table(name = "roles")
 public class Rol {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;	
 }
