989f8b994037de068f57ee4f53df3d5a3725179f
Curso <Curso@DESKTOP-MHK683Q.ipartekaula.com>
2023-12-04 11:39:15 +0100

    Validaciones comprobadas desde consola

--- amazonia2/src/main/java/com/amazonia2/presentacion/consola/PresentacionConsola.java
+++ amazonia2/src/main/java/com/amazonia2/presentacion/consola/PresentacionConsola.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Set;
 
+import com.amazonia2.entidades.Cliente;
 import com.amazonia2.entidades.Producto;
 import com.amazonia2.entidades.Rol;
 import com.amazonia2.globales.Global;
@@ -15,33 +16,55 @@ import jakarta.validation.ValidatorFactory;
 
 public class PresentacionConsola {
 
+	private static final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+	private static final Validator validator = validatorFactory.getValidator();
+
 	public static void main(String[] args) {
-		for(Producto p: Global.UN.listadoProductos()) {
+		for (Producto p : Global.UN.listadoProductos()) {
 			System.out.println(p);
 		}
-		
-		for(Rol r: Global.AN.obtenerTodosLosRoles()) {
+
+		for (Rol r : Global.AN.obtenerTodosLosRoles()) {
 			System.out.println(r);
-			
+
 //			for(Usuario u: r.getUsuarios()) {
 //				System.out.println(u);
 //			}
 		}
-		
-		Producto producto = Producto.builder().codigoBarras("1234567890").nombre("aa").precio(new BigDecimal("10")).unidades(5).fechaCaducidad(LocalDate.now().plusYears(1)).build();
-		
-		ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
-		Validator validator = validatorFactory.getValidator();
-		
-		Set<ConstraintViolation<Producto>> errores = validator.validate(producto);
-		
-		for(ConstraintViolation<Producto> error: errores) {
-			System.out.println("El campo " + error.getPropertyPath() + " " + error.getMessage());
+
+		Producto producto = Producto.builder().codigoBarras("1234567890128").nombre("aa").precio(new BigDecimal("10"))
+				.unidades(5).fechaCaducidad(LocalDate.now().plusYears(1)).build();
+
+		validar(producto);
+
+		Global.AN.insertarProducto(producto);
+
+		Rol rol = Rol.builder().nombre("USER").build();
+		Cliente cliente = Cliente.builder().password("asdf").dni("12345678Z").email("a@b").nombre("asdf").rol(rol)
+				.build();
+
+		if (validar(rol) && validar(cliente)) {
+			Global.FABRICA.obtenerDaoRol().insertar(rol);
+			Global.FABRICA.obtenerDaoUsuario().insertar(cliente);
 		}
+	}
+
+	private static boolean validar(Object objeto) {
+		System.out.println(objeto.getClass().getSimpleName());
 		
-		if(errores.size() == 0) {
-			System.out.println("El producto es válido");
-			System.out.println(producto);
+		Set<ConstraintViolation<Object>> errores = validator.validate(objeto);
+
+		for (ConstraintViolation<Object> error : errores) {
+			System.out.println("\tEl campo " + error.getPropertyPath() + " " + error.getMessage());
+		}
+
+		if (errores.size() == 0) {
+			System.out.println("El objeto es válido");
+			System.out.println(objeto);
+
+			return true;
+		} else {
+			return false;
 		}
 	}
 
