e3052eae9c54c9962b443f08458796794cb57be5
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-12-07 08:21:38 +0100

    Quitamos los elementos de prueba del carrito

--- amazonia2/src/main/java/com/amazonia2/presentacion/backend/listeners/SesionListener.java
+++ amazonia2/src/main/java/com/amazonia2/presentacion/backend/listeners/SesionListener.java
@@ -1,10 +1,6 @@
 package com.amazonia2.presentacion.backend.listeners;
 
-import java.math.BigDecimal;
-
 import com.amazonia2.entidades.Carrito;
-import com.amazonia2.entidades.Producto;
-import com.amazonia2.entidades.Producto.ProductoBuilder;
 
 import jakarta.servlet.annotation.WebListener;
 import jakarta.servlet.http.HttpSessionEvent;
@@ -14,21 +10,21 @@ import jakarta.servlet.http.HttpSessionListener;
 public class SesionListener implements HttpSessionListener {
 
 	public void sessionCreated(HttpSessionEvent se) {
-		Producto p1 = Producto.builder().id(1L).nombre("Producto1").precio(new BigDecimal("1234.12")).unidades(5).build();
-		
-		ProductoBuilder builder = Producto.builder();
-		
-		builder.id(2L);
-		builder.nombre("Producto2");
-		builder.precio(new BigDecimal("234.12"));
-		builder.unidades(7);
-		
-		Producto p2 = builder.build();
+//		Producto p1 = Producto.builder().id(1L).nombre("Producto1").precio(new BigDecimal("1234.12")).unidades(5).build();
+//		
+//		ProductoBuilder builder = Producto.builder();
+//		
+//		builder.id(2L);
+//		builder.nombre("Producto2");
+//		builder.precio(new BigDecimal("234.12"));
+//		builder.unidades(7);
+//		
+//		Producto p2 = builder.build();
 
 		Carrito carrito = Carrito.builder().id(1L).build();
 		
-		carrito.addProducto(p1);
-		carrito.addProducto(p2);
+//		carrito.addProducto(p1);
+//		carrito.addProducto(p2);
 
 		se.getSession().setAttribute("carrito", carrito);
 	}
