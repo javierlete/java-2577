ac207f0aded6754feb5c224449d57c45bb06fa45
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-12-11 09:39:13 +0100

    Spring Starter Project con Lombok y Validation

---
+++ amazonia2spring/src/main/java/com/amazonia2/Amazonia2springApplication.java
@@ -0,0 +1,13 @@
+package com.amazonia2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amazonia2springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amazonia2springApplication.class, args);
+	}
+
+}
---
+++ amazonia2spring/src/test/java/com/amazonia2/Amazonia2springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazonia2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Amazonia2springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
