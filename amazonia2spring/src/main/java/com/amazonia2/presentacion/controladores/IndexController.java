b74546c8b51fbe10e6ffecc3188ab7c77d0a0d2a
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-12-12 10:36:22 +0100

    Pantallas y controlador para las mismas

---
+++ amazonia2spring/src/main/java/com/amazonia2/presentacion/controladores/IndexController.java
@@ -0,0 +1,31 @@
+package com.amazonia2.presentacion.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.amazonia2.logicanegocio.UsuarioNegocio;
+
+@Controller
+@RequestMapping
+public class IndexController {
+	@Autowired
+	private UsuarioNegocio negocio;
+	
+//	@ResponseBody
+	@GetMapping("/index")
+	public String index(Model modelo) {
+//		return negocio.listadoProductos().toString();
+		modelo.addAttribute("productos", negocio.listadoProductos());
+		return "index";
+	}
+	
+//	@ResponseBody
+	@GetMapping("/detalle")
+	public String detalle(Model modelo, Long id) {
+		modelo.addAttribute("producto", negocio.detalleProducto(id));
+		return "detalle";
+	}
+}
