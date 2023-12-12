e6c36fc1eb35016bb25e3d7ca3f75839947d845d
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-12-12 10:35:36 +0100

    @Primary para eliminar ambigüedad

--- amazonia2spring/src/main/java/com/amazonia2/logicanegocio/UsuarioNegocioImpl.java
+++ amazonia2spring/src/main/java/com/amazonia2/logicanegocio/UsuarioNegocioImpl.java
@@ -1,6 +1,7 @@
 package com.amazonia2.logicanegocio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import com.amazonia2.accesodatos.DaoProducto;
@@ -8,6 +9,7 @@ import com.amazonia2.entidades.Producto;
 import com.amazonia2.entidades.Usuario;
 
 @Component
+@Primary
 class UsuarioNegocioImpl implements UsuarioNegocio {
 
 	@Autowired
