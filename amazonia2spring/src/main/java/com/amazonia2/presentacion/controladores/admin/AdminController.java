df60f1d4f1a5b0835fc57f57c1a9096352df04a1
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-12-13 10:31:43 +0100

    Validaciones del formulario de productos

--- amazonia2spring/src/main/java/com/amazonia2/presentacion/controladores/admin/AdminController.java
+++ amazonia2spring/src/main/java/com/amazonia2/presentacion/controladores/admin/AdminController.java
@@ -3,12 +3,15 @@ package com.amazonia2.presentacion.controladores.admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.amazonia2.entidades.Producto;
 import com.amazonia2.logicanegocio.AdminNegocio;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/admin")
@@ -23,7 +26,11 @@ public class AdminController {
 	}
 
 	@PostMapping
-	public String post(Producto producto) {
+	public String post(@Valid Producto producto, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "admin/detalle";
+		}
+		
 		if(producto.getId() == null) {
 			negocio.insertarProducto(producto);
 		} else {
