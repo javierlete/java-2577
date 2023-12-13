ff3016e7e634bd60b08a08c54ac647a34d122dde
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-12-13 09:59:15 +0100

    Mantenimiento de productos

---
+++ amazonia2spring/src/main/java/com/amazonia2/presentacion/controladores/admin/AdminController.java
@@ -0,0 +1,53 @@
+package com.amazonia2.presentacion.controladores.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.amazonia2.entidades.Producto;
+import com.amazonia2.logicanegocio.AdminNegocio;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	@Autowired
+	private AdminNegocio negocio;
+
+	@GetMapping
+	public String index(Model modelo) {
+		modelo.addAttribute("productos", negocio.listadoProductos());
+		return "admin/index";
+	}
+
+	@PostMapping
+	public String post(Producto producto) {
+		if(producto.getId() == null) {
+			negocio.insertarProducto(producto);
+		} else {
+			negocio.modificarProducto(producto);
+		}
+		
+		return "redirect:/admin";
+	}
+
+	@GetMapping("/borrar")
+	public String borrar(Long id) {
+		negocio.borrarProducto(id);
+		
+		return "redirect:/admin";
+	}
+	
+	
+	@GetMapping("/detalle")
+	public String detalle(Model modelo, Long id, Producto producto) {
+		if (id != null) {
+			modelo.addAttribute("producto", negocio.detalleProducto(id));
+		}
+		
+		return "admin/detalle";
+	}
+	
+}
