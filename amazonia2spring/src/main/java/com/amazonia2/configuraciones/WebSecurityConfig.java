8b4ac7098e2c8f1d858554bc5b85482f7067d74b
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-12-15 10:13:04 +0100

    Autenticación y autorización de dos usuarios "hardcodeados"

---
+++ amazonia2spring/src/main/java/com/amazonia2/configuraciones/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.amazonia2.configuraciones;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	// AUTENTICACIÓN
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails admin =
+			 User.withDefaultPasswordEncoder()
+				.username("javier")
+				.password("contra")
+				.roles("ADMIN")
+				.build();
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("pepe")
+				.password("perez")
+				.roles("USER")
+				.build();
+	
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+
+	// AUTORIZACIÓN
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/admin/**").hasRole("ADMIN")
+				.requestMatchers("/detalle").authenticated()
+				.anyRequest().permitAll()
+//				.requestMatchers("/", "/home").permitAll()
+//				.anyRequest().authenticated()
+			)
+			.formLogin((form) -> form
+				//.loginPage("/login")
+				.permitAll()
+			)
+			.logout((logout) -> logout.permitAll());
+
+		return http.build();
+	}
+}
