e3214b84f6f4b154451afaf40f14e6b76cb8c450
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-12-15 09:36:48 +0100

    Control de errores

--- amazonia2spring/src/main/java/com/amazonia2/presentacion/controladores/IndexController.java
+++ amazonia2spring/src/main/java/com/amazonia2/presentacion/controladores/IndexController.java
@@ -1,6 +1,9 @@
 package com.amazonia2.presentacion.controladores;
 
+import java.util.logging.Level;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.amazonia2.logicanegocio.UsuarioNegocio;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.java.Log;
+
+@Log
 @Controller
 @RequestMapping
-public class IndexController {
+public class IndexController implements ErrorController {
 	@Autowired
 	private UsuarioNegocio negocio;
 	
@@ -27,5 +35,17 @@ public class IndexController {
 	public String detalle(Model modelo, Long id) {
 		modelo.addAttribute("producto", negocio.detalleProducto(id));
 		return "detalle";
+	}
+	
+	@GetMapping("/error")
+	public String error(Model modelo, HttpServletRequest request, Exception exception) {
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		log.severe("Status: " + status.toString());
+		log.severe("Mensaje: " + exception.getMessage());
+		log.log(Level.SEVERE, "Detalles de la excepción", exception);
+		
+		modelo.addAttribute("status", status);
+		
+		return "_error";
 	}
 }
