e3de77bad7c719a5c0af902af164625e78574948
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-12-18 09:41:41 +0100

    Seguridad con BCrypt y uso de MySQL como almacén de users y authorities

--- amazonia2spring/src/main/java/com/amazonia2/configuraciones/WebSecurityConfig.java
+++ amazonia2spring/src/main/java/com/amazonia2/configuraciones/WebSecurityConfig.java
@@ -1,32 +1,47 @@
 package com.amazonia2.configuraciones;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-	// AUTENTICACIÓN
+	// https://bcrypt-generator.com/
+	
+	// CODIFICACIÓN DE CONTRASEÑAS BCRYPT
 	@Bean
-	UserDetailsService userDetailsService() {
-		UserDetails admin = User.withDefaultPasswordEncoder()
-			.username("javier").password("contra").roles("ADMIN")
-			.build();
-		UserDetails user = User.withDefaultPasswordEncoder()
-			.username("pepe").password("perez").roles("USER")
-			.build();
-
-		return new InMemoryUserDetailsManager(user, admin);
+	PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder(12);
 	}
+	
+	// AUTENTICACIÓN
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource)
+	  throws Exception {
+	    auth.jdbcAuthentication().dataSource(dataSource);
+	}
+	
+//	@Bean
+//	UserDetailsService userDetailsService() {
+//		UserDetails admin = User.withDefaultPasswordEncoder()
+//			.username("javier").password("contra").roles("ADMIN")
+//			.build();
+//		UserDetails user = User.withDefaultPasswordEncoder()
+//			.username("pepe").password("perez").roles("USER")
+//			.build();
+//
+//		return new InMemoryUserDetailsManager(user, admin);
+//	}
 
 	// AUTORIZACIÓN
 	@Bean
@@ -48,4 +63,20 @@ public class WebSecurityConfig {
 
 		return http.build();
 	}
+	
+//		AUTENTICACIÓN DE MYSQL
+//		----------------------
+//		create table users(
+//			username varchar(50) not null primary key,
+//			password varchar(500) not null,
+//			enabled boolean not null
+//		);
+//
+//		create table authorities (
+//			username varchar(50) not null,
+//			authority varchar(50) not null,
+//			constraint fk_authorities_users foreign key(username) references users(username)
+//		);
+//		create unique index ix_auth_username on authorities (username,authority);
+
 }
