c95c8c58dbf87f28ca9bc923c534c37d4381b0b5
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-12-20 11:32:44 +0100

    Posible configuración a través de Java de una locale concreta

---
+++ amazonia2spring/src/main/java/com/amazonia2/configuraciones/ConfiguracionesGenerales.java
@@ -0,0 +1,19 @@
+package com.amazonia2.configuraciones;
+
+//import java.util.Locale;
+//
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.LocaleResolver;
+//import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class ConfiguracionesGenerales {
+//	@Bean
+//    LocaleResolver localeResolver() {
+//        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+//        Locale locale = new Locale("es", "ES");
+//        sessionLocaleResolver.setDefaultLocale(locale);
+//        return sessionLocaleResolver;
+//    }
+}
