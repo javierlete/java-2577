564774677c215efd068b0dfc6ab2c43e659b7c04
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-12-22 11:38:50 +0100

    Bean de carrito en sesión

--- amazonia2spring/src/main/java/com/amazonia2/configuraciones/ConfiguracionesGenerales.java
+++ amazonia2spring/src/main/java/com/amazonia2/configuraciones/ConfiguracionesGenerales.java
@@ -1,14 +1,26 @@
 package com.amazonia2.configuraciones;
 
+import org.springframework.context.annotation.Bean;
 //import java.util.Locale;
 //
 //import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 //import org.springframework.web.servlet.LocaleResolver;
 //import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.amazonia2.entidades.Carrito;
 
 @Configuration
 public class ConfiguracionesGenerales {
+	@Bean
+	@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
+	Carrito carrito() {
+		return new Carrito();
+	}
+	
 //	@Bean
 //    LocaleResolver localeResolver() {
 //        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
