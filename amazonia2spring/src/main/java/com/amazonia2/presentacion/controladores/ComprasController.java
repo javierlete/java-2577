0aa9c2158236dc84ecf66bd262c02d63a611f3f5
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2023-12-26 08:28:18 +0100

    Quitar modelo de ComprasController

--- amazonia2spring/src/main/java/com/amazonia2/presentacion/controladores/ComprasController.java
+++ amazonia2spring/src/main/java/com/amazonia2/presentacion/controladores/ComprasController.java
@@ -2,7 +2,6 @@ package com.amazonia2.presentacion.controladores;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.amazonia2.entidades.Carrito;
@@ -18,7 +17,7 @@ public class ComprasController {
 	private UsuarioNegocio negocio;
 	
 	@GetMapping("/carrito")
-	public String procesarCarrito(Long idProducto, Model modelo) {
+	public String procesarCarrito(Long idProducto) {
 		if(idProducto != null) {
 			negocio.agregarProductoACarrito(idProducto, carrito);
 		}
