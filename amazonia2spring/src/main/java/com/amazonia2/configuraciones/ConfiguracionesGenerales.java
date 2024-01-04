f4c1eb798f2a337667b234364336e3565ff9c05b
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2024-01-04 08:41:23 +0100

    Gestión global de excepciones

--- amazonia2spring/src/main/java/com/amazonia2/configuraciones/ConfiguracionesGenerales.java
+++ amazonia2spring/src/main/java/com/amazonia2/configuraciones/ConfiguracionesGenerales.java
@@ -1,20 +1,29 @@
 package com.amazonia2.configuraciones;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.amazonia2.entidades.Carrito;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.java.Log;
+
+@Log
 @ControllerAdvice
 @Configuration
 public class ConfiguracionesGenerales {
-	
+
 	private Carrito carrito;
-	
+
 	public ConfiguracionesGenerales(Carrito carrito) {
 		this.carrito = carrito;
 	}
@@ -23,10 +32,36 @@ public class ConfiguracionesGenerales {
 	private void carrito(Model modelo) {
 		modelo.addAttribute(carrito);
 	}
-	
-	// TODO: Crear un interface con todas las operaciones de mapeado en el que se utilice este mapper u otro
+
+	// TODO: Crear un interface con todas las operaciones de mapeado en el que se
+	// utilice este mapper u otro
 	@Bean
-    ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
+	ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	@ExceptionHandler(Exception.class)
+	public String handleApplicationException(Model modelo, Exception exception, HttpServletRequest request) {
+		System.out.println("EXCEPTION----------------------------------");
+
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		
+		if (status != null) {
+			log.severe("Status: " + status.toString());
+		}
+
+		modelo.addAttribute("status", status);
+
+		exception.printStackTrace();
+
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+
+		exception.printStackTrace(pw);
+
+		modelo.addAttribute("exception", exception);
+		modelo.addAttribute("stack", sw.toString());
+
+		return "_error";
+	}
 }
