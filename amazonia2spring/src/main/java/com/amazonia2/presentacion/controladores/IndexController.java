2720a96dcf548c434773997a0b5b8de4fa4e55ee
Curso <Curso@DESKTOP-MHK683Q.mshome.net>
2024-01-08 11:52:02 +0100

    Quitamos el @RequestMapping innecesario

--- amazonia2spring/src/main/java/com/amazonia2/presentacion/controladores/IndexController.java
+++ amazonia2spring/src/main/java/com/amazonia2/presentacion/controladores/IndexController.java
@@ -9,7 +9,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.amazonia2.bibliotecas.Alerta;
 import com.amazonia2.entidades.Cliente;
@@ -23,7 +22,6 @@ import lombok.extern.java.Log;
 
 @Log
 @Controller
-@RequestMapping
 public class IndexController implements ErrorController {
 
 	private static final String LOGIN = "login";
