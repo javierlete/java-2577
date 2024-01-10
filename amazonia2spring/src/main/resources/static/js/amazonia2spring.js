f5d1c696fb21d6ffba669d576b789660b61544e4
Javier Lete <javier@eskura21.com>
2024-01-10 10:47:24 +0100

    Gestión de carrito básica

--- amazonia2spring/src/main/resources/static/js/amazonia2spring.js
+++ amazonia2spring/src/main/resources/static/js/amazonia2spring.js
@@ -2,17 +2,25 @@ const URL_REST = 'http://localhost:8080/api/v2';
 const URL_USUARIO = URL_REST + '/negocio/usuario';
 const URL_PRODUCTOS = URL_USUARIO + '/productos';
 
-$(function() {
+let carrito = JSON.parse(sessionStorage.getItem('carrito'));
+
+if(!carrito) {
+    carrito = [];
+    guardarCarrito();
+}
+
+$(function () {
+
     $('#alerta').hide();
 
     cargarIndex();
 });
 
 function cargarProductos() {
-    $.getJSON(URL_PRODUCTOS, function(productos) {
+    $.getJSON(URL_PRODUCTOS, function (productos) {
         $('#index > div').empty();
-        
-        $.each(productos, function(clave, p) {
+
+        $.each(productos, function (clave, p) {
             $('#index > div').append(`
                 <div class="col">
                     <div class="card h-100">
@@ -23,7 +31,7 @@ function cargarProductos() {
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item d-flex justify-content-between">
                                     <span class="fw-bold">Precio:</span>
-                                    <span>${p.precio}</span>
+                                    <span>${moneda.format(p.precio)}</span>
                                 </li>
                                 <li class="list-group-item d-flex justify-content-between">
                                     <span class="fw-bold">Unidades:</span>
@@ -42,19 +50,89 @@ function cargarProductos() {
                 </div>`);
         });
     });
-    
+
 }
 function verDetalle(id) {
-    $.getJSON(URL_PRODUCTOS + '/' + id, function(producto) {
+    $.getJSON(URL_PRODUCTOS + '/' + id, function (producto) {
         mostrar('detalle');
         console.log(producto);
 
         $('#detalle-nombre').html(producto.nombre);
         $('#detalle-fecha-caducidad').html(producto.fechaCaducidad);
-        $('#detalle-precio').html(producto.precio);
+        $('#detalle-precio').html(moneda.format(producto.precio));
         $('#detalle-unidades').html(producto.unidades);
         $('#detalle-codigo-barras').html(producto.codigoBarras);
+
+        $('#detalle-agregar-carrito').on('click', function (e) {
+            e.preventDefault();
+            agregarACarrito(producto);
+        });
     });
+}
+
+function agregarACarrito(idOProducto) {
+    if (typeof idOProducto === 'number') {
+        const id = idOProducto;
+        $.getJSON(URL_PRODUCTOS + '/' + id, agregarProductoACarrito);
+    } else {
+        const producto = idOProducto;
+        agregarProductoACarrito(producto);
+    }
+}
+
+function agregarProductoACarrito(producto) {
+    const productoEnCarrito = carrito.filter(p => p.id === producto.id)[0];
+    
+    if (productoEnCarrito) {
+        productoEnCarrito.unidades++;
+    } else {
+        producto.unidades = 1;
+        carrito.push(producto);
+    }
+    
+    cargarCarrito();
+}
+
+function cargarCarrito() {
+    guardarCarrito();
+
+    mostrar('carrito');
+
+    $('#carrito tbody').empty()
+
+    let totalGlobal = 0;
+
+    $.each(carrito, function (clave, p) {
+        const total = p.precio * p.unidades;
+        totalGlobal += total;
+
+        $('#carrito tbody').append(`
+            <tr id="${p.id}">
+                <td>${p.nombre}</td>
+                <td class="text-end">${moneda.format(p.precio)}</td>
+                <td class="d-flex justify-content-center">
+                    <div class="input-group mb-3" style="min-width: 11rem; max-width: 11rem">
+                        <button class="btn btn-outline-secondary" type="button"
+                            th:onclick="|quitarUnidad(${p.id})|">
+                            <i class="bi bi-dash-lg"></i>
+                        </button>
+                        <input th:id="|input-${p.id}|" type="text" class="form-control text-end"
+                            placeholder="" aria-label="Example text with button addon"
+                            aria-describedby="button-addon1" value="${p.unidades}">
+                        <button class="btn btn-outline-secondary" type="button"
+                            th:onclick="|agregarUnidad(${p.id})|">
+                            <i class="bi bi-plus-lg"></i>
+                        </button>
+                    </div>
+                </td>
+                <td th:id="|total-${p.id}|" class="text-end">${moneda.format(total)}</td>
+                <td><a onclick="return confirm('¿Estás seguro?')" class="btn btn-sm btn-outline-danger"
+                        href="#" th:href="@{/carrito/borrar(id=${p.id})}"><i
+                            class="bi bi-trash-fill"></i></a></td>
+            </tr>`);
+    });
+
+    $('#total-global').html(moneda.format(totalGlobal));
 }
 
 function cargarIndex() {
@@ -73,8 +151,12 @@ function ocultarTodo() {
 
 function activarDatatables() {
     DataTable('.table-bordered', {
-        language : {
-            url : 'json/es-ES.json',
+        language: {
+            url: 'json/es-ES.json',
         }
     });
+}
+
+function guardarCarrito() {
+    sessionStorage.setItem('carrito', JSON.stringify(carrito));
 }
